11aed14f7e1f9a10b1e69a4d89f442011b4f1e11
刁迎冬 <diaoyingdong@limonergy.com>
2025-12-18 16:50:08 +0800

    时代大厦

---
+++ apps/web-ele/src/views/system/dict/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Page
+    description="我们重新包装了CheckboxGroup、RadioGroup、Select，可以通过options属性传入选项属性数组以自动生成选项"
+    title="表单演示"
+  >
+
+  </Page>
+</template>
+<script setup lang="ts">
+
+import { Page } from "@vben/common-ui";
+</script>
+<style scoped>
+
+</style>
